f8975be517e5339435ccb1a119d01ff68b890f3c
Bunny's Macbook M1 <varun.verma2024@gmail.com>
2025-01-24 18:18:25 +0530

    adds feature 1

--- app/page.jsx
+++ app/page.jsx
@@ -1,6 +1,12 @@
 import React from "react";
 
+const newFeature1 = () => {
+  console.log("This is Feature 1");
+};
+
 const HomePage = () => {
+  // Call feature 1
+  newFeature1();
   return <div>This is new file to commit</div>;
 };
 
